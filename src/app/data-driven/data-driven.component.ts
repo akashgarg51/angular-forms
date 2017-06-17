33c3d0d0b282af3d3714fdab63068268018be500
Akash Garg <akash.garg@sap.com>
2017-06-17 22:24:55 +0530

    Created form using FormBuilder
    Change-Id: I6c5b414d8eeb26cdad416cd2e1252ceba9bded87
    Signed-off-by: Akash Garg <akash.garg@sap.com>

--- src/app/data-driven/data-driven.component.ts
+++ src/app/data-driven/data-driven.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators, FormArray } from "@angular/forms";
+import { FormGroup, FormControl, Validators, FormArray, FormBuilder } from "@angular/forms";
 
 @Component({
     selector: 'data-driven',
@@ -9,8 +9,8 @@ export class DataDrivenComponent {
     myForm: FormGroup
     
     genders = ['male', 'female'];
-    constructor() {
-      this.myForm = new FormGroup({
+    constructor(private formBuilder: FormBuilder) {
+ /*     this.myForm = new FormGroup({
           'userData': new FormGroup({
              'username': new FormControl('Max', Validators.required),
               'email': new FormControl('', [Validators.required, Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")])
@@ -20,7 +20,19 @@ export class DataDrivenComponent {
           'hobbies': new FormArray([
              new FormControl('Cooking', Validators.required)
               ])
-      });    
+      });  */
+        
+      this.myForm = formBuilder.group({
+              'userData': formBuilder.group({
+             'username': ['Max', Validators.required],
+              'email': ['', [Validators.required, Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")]]
+          }),
+          'password': ['', Validators.required],
+          'gender': ['male'],
+          'hobbies': formBuilder.array([
+             ['Cooking', Validators.required]
+              ])
+          });
     }
     
     onAddHobby() {
