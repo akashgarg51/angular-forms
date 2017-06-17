1c74b737ed8b450eeb118cbb53112008a3149562
Akash Garg <akash.garg@sap.com>
2017-06-17 21:57:16 +0530

    Grouping in forms
    Change-Id: I5db5695721cdce3a75c512b8243efb74b348b4f0
    Signed-off-by: Akash Garg <akash.garg@sap.com>

--- src/app/data-driven/data-driven.component.ts
+++ src/app/data-driven/data-driven.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from "@angular/forms"
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
     selector: 'data-driven',
@@ -10,8 +10,10 @@ export class DataDrivenComponent {
     
     constructor() {
       this.myForm = new FormGroup({
-          'username': new FormControl('Max', Validators.required),
-          'email': new FormControl('', [Validators.required, Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")]),
+          'userData': new FormGroup({
+             'username': new FormControl('Max', Validators.required),
+              'email': new FormControl('', [Validators.required, Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")])
+          }),
           'password': new FormControl('', Validators.required)
       });    
     }
